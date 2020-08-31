afc9b6288d53cb38844d5d6adb984c1ecf89eebf
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-08-30 22:00:12 -0300

    feat: dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import useDarkMode from "use-dark-mode";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useDarkMode();
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
